fd40765485ebd209d9674e2ffc795a0788ea0b9d
Jerilyn Widjaja <jerilynwidjaja@gmail.com>
2025-06-03 22:59:44 +0700

    fix: cors error

--- server/app.js
+++ server/app.js
@@ -6,7 +6,11 @@ const db = require('./models');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Import routes
