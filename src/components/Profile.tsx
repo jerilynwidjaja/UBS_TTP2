1d5afcfa669e4917002072cbc89a1ce32ac7dc55
Jerilyn Widjaja <jerilynwidjaja@gmail.com>
2025-06-22 23:12:11 +0800

    fix: update url

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -63,8 +63,8 @@ const Profile: React.FC = () => {
   const fetchCourses = async () => {
     try {
       const [coursesResponse, recommendedResponse] = await Promise.all([
-        axios.get('http://localhost:5000/api/courses'),
-        axios.get('http://localhost:5000/api/courses/recommended')
+        axios.get('http://52.221.205.14:8000/api/courses'),
+        axios.get('http://52.221.205.14:8000/api/courses/recommended')
       ]);
       
       setCourses(coursesResponse.data.courses);
