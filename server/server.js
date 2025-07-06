181b8853670d72df08eeb87600c5c504aa7978d3
Jerilyn Widjaja <jerilynwidjaja@gmail.com>
2025-07-06 22:49:39 +0800

    fix: updated port

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import progressRoutes from './routes/progress.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
